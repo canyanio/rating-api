df0af3fb2f6864c63823e5f24541f2d5a44cbaff
Fabio Tranchitella <fabio@tranchitella.eu>
2020-01-11 15:05:32 +0100

    Use python3 in docker-entrypoint.sh / wait for

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,7 +3,7 @@
 set -e
 
 if ! [ -z "$RATING_API_MONGODB_URI" ]; then
-    HOST_AND_PORT=$(echo $RATING_API_MONGODB_URI | python -c "import sys; from urlparse import urlparse; data = sys.stdin.read().strip(); result = urlparse(data); host = result.netloc.split('@', 1)[-1]; sys.stdout.write(host)")
+    HOST_AND_PORT=$(echo $RATING_API_MONGODB_URI | python3 -c "import sys; from urllib.parse import urlparse; data = sys.stdin.read().strip(); result = urlparse(data); host = result.netloc.split('@', 1)[-1]; sys.stdout.write(host)")
     echo "Waiting for ${HOST_AND_PORT}..."
     /usr/bin/wait-for $HOST_AND_PORT -t 60
 fi
